5f9d2d1f38223e213a48396c8857cfdc98c8f6f7
Valeria <lerazhurova@yandex.ru>
2020-10-19 21:35:59 +0700

    попытка 26

---
+++ Bulls.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>        
+
+
+int main(void)                  
+{                               
+	int madeNumber[4];
+	int allDigits[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int repeatedDig = 0;
+	int RightDigits = 0;
+	int RightPlaces = 0;
+		return 0;                   
+}
